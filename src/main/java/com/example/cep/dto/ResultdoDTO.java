c631252e2f4b5fdc32444daafcf1dc792953916d
bispobr <156684382+bispobr@users.noreply.github.com>
2025-10-11 09:40:51 -0300

    feat: add dto

---
+++ src/main/java/com/example/cep/dto/ResultdoDTO.java
@@ -0,0 +1,18 @@
+package com.example.cep.dto;
+
+public record ResultdoDTO(
+        String cep,
+        String logradouro,
+        String complemento,
+        String unidade,
+        String bairro,
+        String localidade,
+        String uf,
+        String estado,
+        String regiao,
+        String ibge,
+        String gia,
+        String ddd,
+        String siafi
+) {
+}
